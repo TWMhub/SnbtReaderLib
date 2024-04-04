33b2450656a152acd73acbdd99cac33119644143
Depozit <maxtop1924@yandex.ru>
2024-04-04 22:20:56 +0300

    adedd quest

---
+++ SnbtReaderLib/ClassQuest.cpp
@@ -0,0 +1,57 @@
+#include "SnbtReader.h"
+
+Quest::Quest(std::string quest) {
+	this->quest = quest;
+	textAnalyzing(quest);
+}
+
+void Quest::textAnalyzing(std::string text) {
+	size_t firstQuote;
+	size_t secondQuote;
+	for (int i = 0; i < text.length(); i++) {
+		if (text.substr(i, 5) == "title") {
+			firstQuote = text.find("\"", i + 5);
+			if (firstQuote != std::string::npos) {
+				secondQuote = text.find("\"", firstQuote);
+				if (secondQuote != std::string::npos) {
+					setArrayText(Type::title, text.substr(firstQuote + 1, secondQuote - firstQuote - 1));
+				}
+			}
+		}
+		else if (text.substr(i, 8) == "subtitle") {
+			firstQuote = text.find("\"", i + 8);
+			if (firstQuote != std::string::npos) {
+				secondQuote = text.find("\"", firstQuote);
+				if (secondQuote != std::string::npos) {
+					setArrayText(Type::subtitle, text.substr(firstQuote + 1, secondQuote - firstQuote - 1));
+				}
+			}
+		}
+		else if (text.substr(i, 11) == "description") {
+			firstQuote = text.find("\"", i + 11);
+			if (firstQuote != std::string::npos) {
+				secondQuote = text.find("\"", firstQuote);
+				if (secondQuote != std::string::npos) {
+					setArrayText(Type::description, text.substr(firstQuote + 1, secondQuote - firstQuote - 1));
+				}
+			}
+		}
+	}
+}
+
+void Quest::setArrayText(Type type, std::string originalText) {
+	textArray.push_back(Text{ type,originalText });
+}
+
+std::vector<Text> Quest::getTextArray() const {
+	return this->textArray;
+}
+
+void Quest::setTranslatedArray(std::string originalText, std::string translatedText) {
+	for (int i = 0; i < this->textArray.size(); i++) {
+		if (this->textArray[i].getOriginalText() == originalText) {
+			this->textArray[i].setTranslate(translatedText);
+			break;
+		}
+	}
+}
--- SnbtReaderLib/SnbtReader.h
+++ SnbtReaderLib/SnbtReader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 
 enum class Type {
 	title,
@@ -20,4 +21,17 @@ protected:
 	Type type = Type::title;
 	std::string originalText = "";
 	std::string translatedText = "";
+};
+
+class Quest {
+public:
+	Quest(std::string quest);
+	std::vector<Text> getTextArray() const;
+	void setTranslatedArray(std::string originalText, std::string translatedText);
+protected:
+	std::string quest = "";
+	std::vector<Text> textArray;
+
+	void textAnalyzing(std::string text);
+	void setArrayText(Type type, std::string originalText);
 };
